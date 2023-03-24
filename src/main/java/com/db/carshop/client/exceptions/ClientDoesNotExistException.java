32d0de1fe1b1487336639a4b06065469f3674fc0
jessica <jessykrrocha@hotmail.com>
2023-03-24 11:17:09 -0300

    fixes in pr

--- src/main/java/com/db/carshop/client/exceptions/ClientDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.db.carshop.client.exceptions;
-
-public class ClientDoesNotExistException extends RuntimeException{
-    public ClientDoesNotExistException(){
-        super("Client does not exist");
-    }
-}
