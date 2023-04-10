09f8e128b88270f95443f892c02956b33acf7b07
jessica <jessykrrocha@hotmail.com>
2023-04-10 17:14:58 -0300

    without client

--- src/main/java/com/db/carshop/customer/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.db.carshop.customer;
-
-import com.db.carshop.customer.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
